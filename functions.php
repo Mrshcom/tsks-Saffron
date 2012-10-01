046c557bab8aeefa46553a0237b8eb9df6eb0144
farhad-rs <farhad.raeesi@gmail.com>
2012-10-01 17:50:51 +0330

    Start project of CIW [ programming ]

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+	add_theme_support('');
+	register_sidebar(array(
+		'name' => '',
+		'id' => '',
+		'description' => '',
+		'before_widget' => '<nav><li class="widget %2$s">',
+		'after_widget' => "</li></nav>\n",
+	));
+?>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>زعفران سحر خیز</title>
+	<link href="./common-file/reset.css" type="text/css" rel="stylesheet" />
+	<link href="./common-file/style.css" type="text/css" rel="stylesheet" />
+	<script src="./common-file/html5shiv.js" type="text/javascript"></script>
+	<script src="./common-file/zepto.js" type="text/javascript"></script>
+	<script src="./common-file/script.js" type="text/javascript"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="content">
+		
+		</div>
+	</div>
+</body>
+</html>
